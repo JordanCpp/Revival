9f996bee81f670d5c2fb47811acf9d532d6e8b07
Евгений <jordan631@yandex.ru>
2022-08-22 14:40:59 +0400

    Add ImageManager

--- Ext/Ext/Managers/ImageManager.cpp
+++ Ext/Ext/Managers/ImageManager.cpp
@@ -1,1 +1,46 @@
 #include <Ext/Managers/ImageManager.hpp>
+
+Ext::Managers::ImageManager::ImageManager(Ext::Managers::PathManager* pathManager, Ext::Loaders::ImageLoader* imageLoader) :
+	_PathManager(pathManager),
+	_ImageLoader(imageLoader)
+{
+}
+
+Ext::Managers::ImageManager::~ImageManager()
+{
+	for (auto i = _Images.begin(); i != _Images.end(); i++)
+	{
+		delete i->second;
+	}
+}
+
+Ext::Graphics::Image* Ext::Managers::ImageManager::Get(const std::string& dir1, const std::string& dir2, const std::string& dir3, const std::string& file)
+{
+    const char* path = _PathManager->Path(dir1, dir2, dir3, file).c_str();
+
+    auto i = _Images.find(path);
+
+    Ext::Graphics::Image* p = nullptr;
+
+    if (i == _Images.end())
+    {
+        p = _ImageLoader->Load(path);
+        _Images.emplace(path, p);
+    }
+    else
+    {
+        p = i->second;
+    }
+
+    return p;
+}
+
+Ext::Graphics::Image* Ext::Managers::ImageManager::Get(const std::string& dir1, const std::string& dir2, const std::string& file)
+{
+    return Get(dir1, dir2, "", file);
+}
+
+Ext::Graphics::Image* Ext::Managers::ImageManager::Get(const std::string& dir1, const std::string& file)
+{
+    return Get(dir1, "", "", file);
+}
--- Ext/Ext/Managers/ImageManager.hpp
+++ Ext/Ext/Managers/ImageManager.hpp
@@ -1,7 +1,9 @@
 #ifndef Ext_Managers_ImageManager_hpp
 #define Ext_Managers_ImageManager_hpp
 
+#include <Ext/Managers/PathManager.hpp>
 #include <Ext/Loaders/ImageLoader.hpp>
+#include <unordered_map>
 
 namespace Ext
 {
@@ -10,7 +12,15 @@ namespace Ext
         class ImageManager
         {
         public:
+            ImageManager(Ext::Managers::PathManager* pathManager, Ext::Loaders::ImageLoader* imageLoader);
+            ~ImageManager();
+            Ext::Graphics::Image* Get(const std::string& dir1, const std::string& dir2, const std::string& dir3, const std::string& file);
+            Ext::Graphics::Image* Get(const std::string& dir1, const std::string& dir2, const std::string& file);
+            Ext::Graphics::Image* Get(const std::string& dir1, const std::string& file);
         private:
+            Ext::Managers::PathManager* _PathManager;
+            Ext::Loaders::ImageLoader* _ImageLoader;
+            std::unordered_map<std::string, Ext::Graphics::Image*> _Images;
         };
     }
 }
