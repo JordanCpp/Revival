00691e532f503f62b027660a29dc96f58321d79e
Евгений <jordan631@yandex.ru>
2022-08-26 15:10:57 +0400

    Add test Point2u

--- Ext/Test/Graphics/Point2u.cpp
+++ Ext/Test/Graphics/Point2u.cpp
@@ -1,7 +1,7 @@
 #include <Ext/Core/Tester.hpp>
 #include <Ext/Graphics/Point2u.hpp>
 
-void TestExtGraphicsPoint2uEmptyInit()
+void TestExtGraphicsPoint2uInitDefault()
 {
 	Ext::Graphics::Point2u point;
 
@@ -9,9 +9,18 @@ void TestExtGraphicsPoint2uEmptyInit()
 	EXT_TESTER_EQUAL(point.PosY(), 0);
 }
 
+void TestExtGraphicsPoint2uInitValue()
+{
+	Ext::Graphics::Point2u point(15, 30);
+
+	EXT_TESTER_EQUAL(point.PosX(), 15);
+	EXT_TESTER_EQUAL(point.PosY(), 30);
+}
+
 int main(int argc, char* argv[])
 {
-	TestExtGraphicsPoint2uEmptyInit();
+	TestExtGraphicsPoint2uInitDefault();
+	TestExtGraphicsPoint2uInitValue();
 
 	return 0;
 }
---
+++ Test.bat
@@ -0,0 +1,3 @@
+for %%f in (out\build\x86-Release\Ext\Test\Graphics\*.exe) do call %%f
+
+pause
