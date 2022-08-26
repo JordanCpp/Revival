5dd9c545bd9d12cd42c1c64eeafa6391b7d8c1e6
Евгений <jordan631@yandex.ru>
2022-08-26 15:34:37 +0400

    Add test Color

---
+++ Ext/Test/Graphics/Color.cpp
@@ -0,0 +1,41 @@
+#include <Ext/Core/Tester.hpp>
+#include <Ext/Graphics/Color.hpp>
+
+void TestExtGraphicsColorInitDefault()
+{
+	Ext::Graphics::Color color;
+
+	EXT_TESTER_EQUAL(color.Red(), 0);
+	EXT_TESTER_EQUAL(color.Green(), 0);
+	EXT_TESTER_EQUAL(color.Blue(), 0);
+	EXT_TESTER_EQUAL(color.Alpha(), 255);
+}
+
+void TestExtGraphicsColorInitValue()
+{
+	Ext::Graphics::Color color(1, 2, 3);
+
+	EXT_TESTER_EQUAL(color.Red(), 1);
+	EXT_TESTER_EQUAL(color.Green(), 2);
+	EXT_TESTER_EQUAL(color.Blue(), 3);
+	EXT_TESTER_EQUAL(color.Alpha(), 255);
+}
+
+void TestExtGraphicsColorInitValue2()
+{
+	Ext::Graphics::Color color(1, 2, 3, 4);
+
+	EXT_TESTER_EQUAL(color.Red(), 1);
+	EXT_TESTER_EQUAL(color.Green(), 2);
+	EXT_TESTER_EQUAL(color.Blue(), 3);
+	EXT_TESTER_EQUAL(color.Alpha(), 4);
+}
+
+int main(int argc, char* argv[])
+{
+	TestExtGraphicsColorInitDefault();
+	TestExtGraphicsColorInitValue();
+	TestExtGraphicsColorInitValue2();
+
+	return 0;
+}
