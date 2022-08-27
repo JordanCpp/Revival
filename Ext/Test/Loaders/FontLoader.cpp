e9e24c89d498ea511f43a6eef15522532a92c8a8
Евгений <jordan631@yandex.ru>
2022-08-27 23:28:48 +0400

    Fix FontLoader test

--- Ext/Test/Loaders/FontLoader.cpp
+++ Ext/Test/Loaders/FontLoader.cpp
@@ -4,7 +4,7 @@
 
 void TestExtGraphicsFontLoaderLoad()
 {
-	EXT_TESTER_EXCEPTION(Ext::Core::Initializer initializer;);
+	Ext::Core::Initializer initializer;
 
 	Ext::Loaders::FontLoader fontLoader;
 
@@ -13,7 +13,7 @@ void TestExtGraphicsFontLoaderLoad()
 	EXT_TESTER_EXCEPTION(font = fontLoader.Load("TestFiles/FalloutRegular.ttf", 7));
 
 	EXT_TESTER_EQUAL(font->Get() != nullptr);
-	EXT_TESTER_EQUAL(font->Size() == 9);
+	EXT_TESTER_EQUAL(font->Size() == 7);
 }
 
 int main(int argc, char* argv[])
