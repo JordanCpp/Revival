b01340bb4629b2a554b96bf245c78f41475b5cd0
Евгений <jordan631@yandex.ru>
2022-08-27 16:17:42 +0400

    Update test Perk

--- Sources/Fallout/Test/Gameplay/Perk.cpp
+++ Sources/Fallout/Test/Gameplay/Perk.cpp
@@ -1,23 +1,24 @@
 #include <Ext/Core/Tester.hpp>
 #include <Fallout/Gameplay/Perk.hpp>
+#include <Fallout/Critters/Stats.hpp>
 
 void TestGameplayPerkInitDefault()
 {
 	Fallout::Gameplay::Perk perk;
 
 	EXT_TESTER_EQUAL(Fallout::Gameplay::Perk::Max > 0);
-	EXT_TESTER_EQUAL(perk.Bonus(5, 20) == 0);
+	EXT_TESTER_EQUAL(perk.Bonus(Fallout::Critters::Stats::Strength, 20) == 0);
 }
 
 void TestGameplayPerkAddValues()
 {
 	Fallout::Gameplay::Perk perk;
 
-	perk.Add(Fallout::Gameplay::Attribute(5, 5, 15));
+	perk.Add(Fallout::Gameplay::Attribute(Fallout::Critters::Stats::Strength, 5, 15));
 
-	int n = perk.Bonus(5, 100);
+	int n = perk.Bonus(Fallout::Critters::Stats::Strength, 100);
 
-	EXT_TESTER_EQUAL(perk.Bonus(5, 100) == 20);
+	EXT_TESTER_EQUAL(perk.Bonus(Fallout::Critters::Stats::Strength, 100) == 20);
 }
 
 int main(int argc, char* argv[])
